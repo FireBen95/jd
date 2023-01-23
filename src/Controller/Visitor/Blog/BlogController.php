0ac263842238136c9a9e4293c9fbffc462b36a51
Benoît <votreemail@gmail.com>
2023-01-23 16:26:27 +0100

    Mise en place du filtre des articles

---
+++ src/Controller/Visitor/Blog/BlogController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Controller\Visitor\Blog;
+
+use App\Entity\Tag;
+use App\Entity\Post;
+use App\Entity\Category;
+use App\Repository\TagRepository;
+use App\Repository\PostRepository;
+use App\Repository\CategoryRepository;
+use Knp\Component\Pager\PaginatorInterface;
+use Doctrine\ORM\Tools\Pagination\Paginator;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class BlogController extends AbstractController
+{
+    #[Route('/blog/all-posts', name: 'visitor.blog.index')]
+    public function index(
+        CategoryRepository $categoryRepository,
+        TagRepository $tagRepository,
+        PostRepository $postRepository,
+        PaginatorInterface $paginator,
+        Request $request
+    ): Response
+    {
+    
+
+        $categories = $categoryRepository->findAll();
+        $tags       = $tagRepository->findAll();
+        $posts      = $postRepository->findBy(['isPublished' => true]);
+
+        $posts_paginated = $paginator->paginate(
+            $posts, /* query NOT result */
+            $request->query->getInt('page', 1), /*page number*/
+            10 /*limit per page*/
+        );
+
+        return $this->render('pages/visitor/blog/index.html.twig', compact('categories', 'tags', 'posts_paginated'));
+    }
+
+
+    #[Route('/blog/post/{id<\d+>}/{slug}', name: 'visitor.blog.post.show')]
+    public function show(
+        Post $post
+    ): Response
+    {
+        return $this->render('pages/visitor/blog/show.html.twig', compact('post'));
+    }
+
+
+    #[Route('/blog/posts/filter_by_category/{id<\d+>}/{slug}', name: 'visitor.blog.posts.filter_by_category')]
+    public function filterByCategory(
+        Category $category,
+        CategoryRepository $categoryRepository,
+        TagRepository $tagRepository,
+        PostRepository $postRepository,
+        PaginatorInterface $paginator,
+        Request $request
+    ): Response
+    {
+        $categories = $categoryRepository->findAll();
+        $tags       = $tagRepository->findAll();
+        $posts      = $postRepository->filterPostsByCategory($category->getId());
+        
+        $posts_paginated = $paginator->paginate(
+            $posts, /* query NOT result */
+            $request->query->getInt('page', 1), /*page number*/
+            10 /*limit per page*/
+        );
+
+        return $this->render('pages/visitor/blog/index.html.twig', compact('categories', 'tags' ,'posts_paginated'));
+    }
+
+
+    #[Route('/blog/posts/filter_by_tag/{id<\d+>}/{slug}', name: 'visitor.blog.posts.filter_by_tag')]
+    public function filterByTag(
+        Tag $tag,
+        CategoryRepository $categoryRepository,
+        TagRepository $tagRepository,
+        PostRepository $postRepository,
+        PaginatorInterface $paginator,
+        Request $request
+        ) : Response
+    {
+        $categories = $categoryRepository->findAll();
+        $tags        = $tagRepository->findAll();
+        $posts      = $postRepository->filterPostsByCategory($tag);
+        
+        $posts_paginated = $paginator->paginate(
+            $posts, /* query NOT result */
+            $request->query->getInt('page', 1), /*page number*/
+            10 /*limit per page*/
+        );
+
+        return $this->render("pages/visitor/blog/index.html.twig", compact('categories', 'tags', 'posts_paginated'));
+    }
+}
---
+++ src/Repository/PostRepository.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Post;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\Query\Expr\Select;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Post>
+ *
+ * @method Post|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Post|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Post[]    findAll()
+ * @method Post[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class PostRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Post::class);
+    }
+
+    public function save(Post $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->persist($entity);
+
+        if ($flush) 
+        {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+    public function remove(Post $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->remove($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+
+    public function filterPostsByCategory($category_id) : array
+    {
+        return $this->createQueryBuilder('p')
+             ->innerJoin('p.category', 'c')
+             ->andWhere('p.isPublished = :val')
+             ->andWhere('p.category = :category_id')
+             ->setParameter('val', true)
+             ->setParameter('category_id', $category_id)
+             ->orderBy('p.id', "ASC")
+             ->getQuery()
+             ->getResult()
+        ;
+    }
+
+
+    public function filterPostsByTag($tag) : array
+    {
+        return $this->createQueryBuilder('p')
+                    ->join('p.tags', 't')
+                    ->select('p')
+                    ->where('t.id = :id')
+                    // ->addSelect('t')
+                    ->andWhere('p.isPublished = :val')
+                    ->setParameter('id', $tag->getId())
+                    ->setParameter('val', true)
+                    ->getQuery()
+                    ->getResult();
+    }
+
+//    /**
+//     * @return Post[] Returns an array of Post objects
+//     */
+//    public function findByExampleField($value): array
+//    {
+//        return $this->createQueryBuilder('p')
+//            ->andWhere('p.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->orderBy('p.id', 'ASC')
+//            ->setMaxResults(10)
+//            ->getQuery()
+//            ->getResult()
+//        ;
+//    }
+
+//    public function findOneBySomeField($value): ?Post
+//    {
+//        return $this->createQueryBuilder('p')
+//            ->andWhere('p.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->getQuery()
+//            ->getOneOrNullResult()
+//        ;
+//    }
+}
